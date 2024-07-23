8fc227b57fbdef366baf057a30427afc22c09283
Michael Schmidt <mschmidty@gmail.com>
2024-07-22 22:08:34 -0600

    added some window movement and windows R term

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,11 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.background = "light"
 
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
 -- vim.opt.timeoutlen = 1000
 
 vim.opt.swapfile = false
