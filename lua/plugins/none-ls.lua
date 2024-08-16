6d9e5a5a48cb08729ac1fe48c97548496bc884cc
Mike Schmidt (Schmidty) <mschmidty@gmail.com>
2024-08-15 21:34:53 -0600

    got R lint/formatting working

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,25 @@
-return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim"
-  },
-  config = function ()
-    local null_ls = require("null-ls")
-
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.r_language_server
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
-}
+  return {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    requires = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.styler,
+        },
+        on_attach = function(client, bufnr)
+          if client.server_capabilities.documentFormattingProvider then
+            vim.api.nvim_command("augroup Format")
+            vim.api.nvim_command("autocmd! * <buffer>")
+            vim.api.nvim_command("autocmd BufWritePre <buffer> lua vim.lsp.buf.format()")
+            vim.api.nvim_command("augroup END")
+          end
+        end,
+      })
+    end, 
+  }
