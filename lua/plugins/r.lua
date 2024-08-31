bf9a1794c58b88c9b25ca88e3d4f6039265a5a70
Michael Schmidt <mschmidty@gmail.com>
2024-08-31 13:22:25 -0600

    still trying to get visidata to work on windows

--- lua/plugins/r.lua
+++ lua/plugins/r.lua
@@ -1,51 +1,87 @@
 return {
-  {
-        "R-nvim/R.nvim",
-        config = function ()
-            -- Create a table with the options to be passed to setup()
-            local opts = { 
-                R_args = {"--quiet", "--no-save"},
-                -- Below is for windows work maching only, should figure out how to only apply on windows.
-                hook = {
-                    on_filetype = function ()
-                        -- This function will be called at the FileType event
-                        -- of files supported by R.nvim. This is an
-                        -- opportunity to create mappings local to buffers.
-                        vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
-                        vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
-                        vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>m", "<Cmd>lua require('r.send').cmd('tar_make()')<CR>", {})
-                        vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>le", "<Cmd>lua require('r.send').cmd('targets::tar_load_everything()')<CR>", {})
-                        vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>ll", "<Cmd>lua require('r.run').action('tar_load')<CR>", {})
-                        vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>tm", "<Cmd>lua require('r.run').action('tar_make')<CR>", {})
-                        vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>tr", "<Cmd>lua require('r.run').action('tar_read')<CR>", {})
-                        vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>ss", ":normal! viw<CR><Plug>RDSendSelection", {})
-                    end
-                },
-                csv_app = "terminal:vd",
-                min_editor_width = 72,
-                rconsole_width = 78,
-                rmdchunk = "``",
-                disable_cmds = {
-                        "RClearConsole",
-                        "RCustomStart",
-                        "RSPlot",
-                        "RSaveClose",
-                    },
-                }
-                -- Check if the environment variable "R_AUTO_START" exists.
-                -- If using fish shell, you could put in your config.fish:
-                -- alias r "R_AUTO_START=true nvim"
-                if vim.env.R_AUTO_START == "true" then
-                    opts.auto_start = 1
-                    opts.objbr_auto_start = true
-                end
-                if vim.loop.os_uname().sysname == "Windows_NT" then
-                  opts.R_path = 'C:\\Users\\MichaelSchmidt2\\AppData\\Local\\Programs\\R\\R-4.3.0\\bin\\x64'
-                  opts.csv_app = "terminal:C:\\users\\michaelschmidt2\\appdata\\local\\programs\\python\\python311\\Scripts\\vd"
-                end
-                require("r").setup(opts)
-                vim.g.rout_follow_colorscheme = true
-            end,
-        lazy = false
-  },
+	{
+		"R-nvim/R.nvim",
+		config = function()
+			-- Create a table with the options to be passed to setup()
+			local opts = {
+				R_args = { "--quiet", "--no-save" },
+				-- Below is for windows work maching only, should figure out how to only apply on windows.
+				hook = {
+					on_filetype = function()
+						-- This function will be called at the FileType event
+						-- of files supported by R.nvim. This is an
+						-- opportunity to create mappings local to buffers.
+						vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
+						vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+						vim.api.nvim_buf_set_keymap(
+							0,
+							"n",
+							"<LocalLeader>m",
+							"<Cmd>lua require('r.send').cmd('tar_make()')<CR>",
+							{}
+						)
+						vim.api.nvim_buf_set_keymap(
+							0,
+							"n",
+							"<LocalLeader>le",
+							"<Cmd>lua require('r.send').cmd('targets::tar_load_everything()')<CR>",
+							{}
+						)
+						vim.api.nvim_buf_set_keymap(
+							0,
+							"n",
+							"<LocalLeader>ll",
+							"<Cmd>lua require('r.run').action('tar_load')<CR>",
+							{}
+						)
+						vim.api.nvim_buf_set_keymap(
+							0,
+							"n",
+							"<LocalLeader>tm",
+							"<Cmd>lua require('r.run').action('tar_make')<CR>",
+							{}
+						)
+						vim.api.nvim_buf_set_keymap(
+							0,
+							"n",
+							"<LocalLeader>tr",
+							"<Cmd>lua require('r.run').action('tar_read')<CR>",
+							{}
+						)
+						vim.api.nvim_buf_set_keymap(
+							0,
+							"n",
+							"<LocalLeader>ss",
+							":normal! viw<CR><Plug>RDSendSelection",
+							{}
+						)
+					end,
+				},
+				csv_app = "terminal:vd",
+				min_editor_width = 72,
+				rconsole_width = 78,
+				rmdchunk = "``",
+				disable_cmds = {
+					"RClearConsole",
+					"RCustomStart",
+					"RSPlot",
+					"RSaveClose",
+				},
+			}
+			-- Check if the environment variable "R_AUTO_START" exists.
+			-- If using fish shell, you could put in your config.fish:
+			-- alias r "R_AUTO_START=true nvim"
+			if vim.env.R_AUTO_START == "true" then
+				opts.auto_start = 1
+				opts.objbr_auto_start = true
+			end
+			if vim.loop.os_uname().sysname == "Windows_NT" then
+				opts.R_path = "C:\\Users\\MichaelSchmidt2\\AppData\\Local\\Programs\\R\\R-4.3.0\\bin\\x64"
+				opts.csv_app = "termain:visidata"
+			end
+			require("r").setup(opts)
+			vim.g.rout_follow_colorscheme = true
+		end,
+		lazy = false,
+	},
 }
